e2c4fe3faca2b84bf578d2ba87baefff6adf189b
fau <witold.pilat@gmail.com>
2017-03-13 17:38:50 +0100

    Smoother Seeker Drone orbiting around player

--- code/game/w_force.c
+++ code/game/w_force.c
@@ -4093,7 +4093,7 @@ void SeekerDroneUpdate(gentity_t *self)
 		VectorCopy(self->client->ps.origin, elevated);
 		elevated[2] += 40;
 
-		angle = ((level.time / 12) & 255) * (float) (M_PI * 2 / 255); //magical numbers make magic happen
+		angle = (level.time % 3072) * (float) (M_PI * 2 / 3072); //magical numbers make magic happen
 		dir[0] = cosf(angle) * 20;
 		dir[1] = sinf(angle) * 20;
 		dir[2] = cosf(angle) * 5;
@@ -4140,7 +4140,7 @@ void SeekerDroneUpdate(gentity_t *self)
 
 		elevated[2] -= 55-prefig;
 
-		angle = ((level.time / 12) & 255) * (float) (M_PI * 2 / 255); //magical numbers make magic happen
+		angle = (level.time % 3072) * (float) (M_PI * 2 / 3072); //magical numbers make magic happen
 		dir[0] = cosf(angle) * 20;
 		dir[1] = sinf(angle) * 20;
 		dir[2] = cosf(angle) * 5;
@@ -4208,7 +4208,7 @@ void SeekerDroneUpdate(gentity_t *self)
 		VectorCopy(self->client->ps.origin, elevated);
 		elevated[2] += 40;
 
-		angle = ((level.time / 12) & 255) * (float) (M_PI * 2 / 255); //magical numbers make magic happen
+		angle = (level.time % 3072) * (float) (M_PI * 2 / 3072); //magical numbers make magic happen
 		dir[0] = cosf(angle) * 20;
 		dir[1] = sinf(angle) * 20;
 		dir[2] = cosf(angle) * 5;
