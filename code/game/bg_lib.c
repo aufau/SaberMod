b984e8ea65c8edb1549ad3e9bfba38d1b22ba348
fau <witold.pilat@gmail.com>
2017-04-29 14:08:33 +0200

    bg_lib.c: Fix strncmp

--- code/game/bg_lib.c
+++ code/game/bg_lib.c
@@ -249,17 +249,20 @@ int strcmp( const char *string1, const char *string2 ) {
 }
 
 int strncmp (const char *s1, const char *s2, size_t n) {
+	int		c1, c2;
+
 	do {
-		if (n == 0) {
+		c1 = *s1++;
+		c2 = *s2++;
+
+		if (!n--) {
 			return 0;		// strings are equal until end point
 		}
-		n--;
-		if (*s1 != *s2) {
-			return (int)*s1 - (int)*s2;
+
+		if (c1 != c2) {
+			return c1 < c2 ? -1 : 1;
 		}
-		s1--;
-		s2--;
-	} while (*s1);
+	} while (c1);
 
 	return 0;		// strings are equal
 }
