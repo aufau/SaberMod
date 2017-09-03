c0337571856ce4ab81a503f79b7ef90132ade3c5
fau <witold.pilat@gmail.com>
2017-09-03 18:18:19 +0200

    Fix copyright notices to specify exactly GPL2 as in README-raven.txt

--- code/game/g_log.c
+++ code/game/g_log.c
@@ -4,10 +4,9 @@ This file is part of SaberMod - Star Wars Jedi Knight II: Jedi Outcast mod.
 
 Copyright (C) 2015-2017 Witold Pilat <witold.pilat@gmail.com>
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or modify it
+under the terms and conditions of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
