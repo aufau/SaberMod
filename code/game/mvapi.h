6aec382faf9860728ef8cd374e46c125c3caa188
fau <witold.pilat@gmail.com>
2017-09-05 21:17:58 +0200

    mvapi.h: Update to draft MVAPI 3

--- code/game/mvapi.h
+++ code/game/mvapi.h
@@ -14,8 +14,8 @@
 // MV_MIN_VERSION is the minimum required JK2MV version which implements this API-Level.
 // All future JK2MV versions are guaranteed to implement this API-Level.
 // ----------------------------------------------------------------------------------------- //
-#define MV_APILEVEL 2
-#define MV_MIN_VERSION "1.2"
+#define MV_APILEVEL 3
+#define MV_MIN_VERSION "1.4"
 // ----------------------------------------------------------------------------------------- //
 
 // ----------------------------------------- SHARED ---------------------------------------- //
@@ -31,7 +31,8 @@ typedef enum {
 	MVFIX_TURRETCRASH         = (1 << 4),
 	MVFIX_CHARGEJUMP          = (1 << 5),
 	MVFIX_SPEEDHACK           = (1 << 6),
-	MVFIX_SABERSTEALING	      = (1 << 7),
+	MVFIX_SABERSTEALING       = (1 << 7),
+	MVFIX_PLAYERGHOSTING      = (1 << 8),
 
 	/* CGAME */
 	MVFIX_WPGLOWING           = (1 << 16),
@@ -46,11 +47,23 @@ typedef enum {
 
 // ******** SYSCALLS ******** //
 
-// qboolean trap_MVAPI_ControlFixes(mvfix_t fixes);
+// void trap_R_Font_DrawString(int ox, int oy, const char *text, const float *rgba, int setIndex, int iCharLimit, float hScale, float vScale);
+#define MVAPI_R_FONT_DRAWSTRING 700              /* asm: -701 */
+
+// int trap_R_Font_StrLenPixels(const char *text, qhandle_t iFontIndex, float hScale, float vScale);
+#define MVAPI_R_FONT_STRLENPIXELS 701            /* asm: -702 */
+
+// int trap_R_Font_HeightPixels(qhandle_t iFontIndex, float hScale, float vScale);
+#define MVAPI_R_FONT_STRHEIGHTPIXELS 702         /* asm: -703 */
+
+// qboolean trap_MVAPI_ControlFixes(int fixes);
 #define MVAPI_CONTROL_FIXES 703                  /* asm: -704 */
 
 // mvversion_t trap_MVAPI_GetVersion(void);
 #define MVAPI_GET_VERSION 704                    /* asm: -705 */
+
+// void trap_MVAPI_R_DrawTransformPic(float x, float y, float (*m)[2][2], float s1, float t1, float s2, float t2, qhandle_t hShader);
+#define MVAPI_R_DRAWTRANSFORMPIC 707             /* asm: -706 */
 
 // ******** VMCALLS ******** //
 
@@ -106,6 +119,11 @@ typedef struct {
 // ------------------------------------------ UI ------------------------------------------- //
 
 #define MVSORT_CLIENTS_NOBOTS 5
+
+// -------------------------------------- UI & CGAME --------------------------------------- //
+
+// void trap_R_AddRefEntityToScene2(const refEntity_t *re);
+#define MVAPI_R_ADDREFENTITYTOSCENE2 706         /* asm: -707 */
 
 // ----------------------------------------------------------------------------------------- //
 
