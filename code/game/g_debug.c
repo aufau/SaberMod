9beeee3235cffb0d44e2484ba176035ddb73e579
fau <witold.pilat@gmail.com>
2017-10-14 00:18:05 +0200

    G_EntityCheckRep: Get rid of health check for players

--- code/game/g_debug.c
+++ code/game/g_debug.c
@@ -51,7 +51,7 @@ void G_EntityCheckRep(const gentity_t *ent)
 
 	if (ent->client) {
 		assert(level.clients + num == ent->client);
-		assert(ent->health == ent->client->ps.stats[STAT_HEALTH]);
+		// assert(ent->health == ent->client->ps.stats[STAT_HEALTH]);
 		assert(ent->blameEntityNum == num);
 		G_ClientCheckRep(ent->client);
 	}
