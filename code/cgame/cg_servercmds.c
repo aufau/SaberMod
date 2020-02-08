ce66787a20963a847383c472d6c6ff35283d16b7
fau <witold.pilat@gmail.com>
2020-02-08 14:43:12 +0100

    Don't turn off cg_thirdPerson on map restart
    This was a leftover from GT_SINGLE_PLAYER - spwin etc.

--- code/cgame/cg_servercmds.c
+++ code/cgame/cg_servercmds.c
@@ -716,7 +716,6 @@ static void CG_MapRestart( void ) {
 		trap_S_StartLocalSound( cgs.media.countFightSound, CHAN_ANNOUNCER );
 		CG_CenterPrint( CG_GetStripEdString("SVINGAME", "BEGIN_DUEL"), 120 );
 	}
-	trap_Cvar_Set("cg_thirdPerson", "0");
 }
 
 #ifdef MISSIONPACK
